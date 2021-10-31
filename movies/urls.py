7039a486939703c3cc86ce66b1f29401b757bbdd
Robera16 <roberaworknehrobwork@gmail.com>
2021-10-31 16:13:22 +0300

    collect static files

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.urls import path
 from . import views
 # mapping a url endpoint to view function
+
+app_name = 'movies'
 urlpatterns = [
-    path('', views.index, name='index') 
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
--- vidly/settings.py
+++ vidly/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies.apps.MoviesConfig'
-
+    'movies.apps.MoviesConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'vidly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
